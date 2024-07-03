927c70b6fbb69bf57ac91158d3533eeb8669d39d
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-07-03 07:15:54 -0600

    chore: Refactor SeeProductoBtn component to handle black and white color options

--- src/components/SeeProductoBtn.tsx
+++ src/components/SeeProductoBtn.tsx
@@ -1,9 +1,17 @@
 
+type SeeProductoBtnProps = {
+  color?: string;
+}
+export default function SeeProductoBtn({ color = 'default' }: SeeProductoBtnProps) {
+  const className : { [key: string]: string } = {
+    default: "bg-custom-orange text-white",
+    white: "bg-transparent border border-zinc-950 text-zinc-950 ",
+    black: "bg-zinc-950 text-white"
+  };
 
-export default function SeeProductoBtn() {
   return (
-    <button className="bg-custom-orange text-white font-bold w-fit px-6 py-3">
-    SEE PRODUCT 
+    <button className={`${className[color]} font-bold w-fit px-6 py-3`}>
+      SEE PRODUCT
     </button>
   )
 }
