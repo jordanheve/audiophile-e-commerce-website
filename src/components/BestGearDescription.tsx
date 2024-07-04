e83b1395f7fcdecf2e863286e6c97b00ba982d48
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-07-04 06:38:22 -0600

    chore: Add BestGearDescription component to home page

---
+++ src/components/BestGearDescription.tsx
@@ -0,0 +1,18 @@
+import bestGearImg from '/assets/shared/desktop/image-best-gear.jpg';
+export default function BestGearDescription() {
+  return (
+    <section className='p-4 text-center flex flex-col gap-6 my-24' >
+        <div >
+        <img src={bestGearImg} alt="a guy wearing headphones" className='rounded-lg w-full h-full object-cover aspect-square '/>
+        </div>
+        <h3 className="uppercase text-zinc-950 font-semibold text-3xl">
+        Bringing you the  <br />
+        <span className="text-custom-orange"> best </span>
+         audio gear
+        </h3>
+        <p className="text-zinc-600">
+        Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
+        </p>
+    </section>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import CategoryOptions from "../components/CategoryOptions";
 import MainSection from "../components/home/MainSection";
+import BestGearDescription from "../components/BestGearDescription";
 
 export default function Home() {
   return (
     <div>
       <MainSection />
       <CategoryOptions />
+      <BestGearDescription />
     </div>
   )
 }
